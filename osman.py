e50e9e2975521549e2f50e2d6004ef4326efd1df
poldanf22 <108033529+poldanf22@users.noreply.github.com>
2023-09-17 03:37:55 +0700

    url ke lokasi

--- osman.py
+++ osman.py
@@ -28,7 +28,7 @@ if authentication_status == False:
 if authentication_status == None:
     st.warning("Silahkan masukan username dan kode akses")
 
-url = "https://osman2-8bdgvgq3z54.streamlit.app/"
+url = "https://osmansiplok.streamlit.app/"
 
 if authentication_status:
     authenticator.logout("Logout", "sidebar")
